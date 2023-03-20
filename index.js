b20c7078ae78688217869dae056a40a6f5c5a580
francisco perez <frperez032219@gmail.com>
2023-03-20 06:53:26 -0700

    finished

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
 const fs = require('fs');
 const Employee = require('./lib/Employee');
-
+const employees = []
 //Placeing it in a function allows users to keep creating new employees
 function newEmployee() {
     //ask user for employee data
@@ -13,7 +13,7 @@ function newEmployee() {
         {
             type: 'list',
             name: 'position',
-            message: 'What position is this emplyee?',
+            message: 'What position is this employee?',
             choices: [
                 'Manager',
                 'Intern',
@@ -31,13 +31,13 @@ function newEmployee() {
             message: 'What is the employee email?'
         },
         {
-            type: 'list',
+            type: 'input',
             name: 'id',
-            message: 'What is the employee ID?'
-        },
+            message: 'What is the employee ID?',
+        }
     
         //take data and the chosen role and create a new employee object with that role and data
-    ]).then((position, email, id , name) => {
+    ]).then(({position, email, id , name}) => {
         switch(position) {
     
             case 'Manager':
@@ -46,43 +46,139 @@ function newEmployee() {
                     {
                         type:   'input',
                         name: 'officeNumber',
-                        message: 'What is the office number?'
+                        message: 'What is the office number?',
                     }
-                ]).then(({officeNumber}) => {
+                ]).then(({ officeNumber }) => {
                     employees.push(new Manager(
                         name,
-                        id,
                         email,
+                        id,
                         officeNumber
-                    ))
+                    ));
+                    addEmployee();
                     })
             break;
     
             case 'Intern':
                 //ask for school
+                inquirer.prompt ([
+                    {
+                        type: 'input',
+                        name: 'school',
+                        message: 'What is the school you attend?',
+                    }
+                ]).then(({ school }) => {
+                    employees.push(new Intern(
+                        name,
+                        id,
+                        email,
+                        school
+                    ))
+                    addEmployee()
+                })
             break;
     
             case 'Engineer':
                 //ask for github
+                inquirer.prompt ([
+                    {
+                        type: 'input',
+                        name: 'github',
+                        message: 'What is the employees github account?',
+                    },
+                ]).then(({ github }) => {
+                    employees.push(new Engineer(
+                        name,
+                        id,
+                        email,
+                        github
+                    ))
+                    addEmployee()
+                })
             break;
     
             default:
                 //uh oh
         }
+        });
+    }
 
-        //ask user if they want to add more employees
-        inquirer.prompt([
-            {
-                type: 'confirm',
-                name: 'more',
-                message: 'Do you want to create a new employee?'
-            }
-            //if answer is yes(true) then create a new employee if false then create HTML file with the new employees
-        ]).then(({ more }) => {
-            if(more) newEmployee()
-            else console.log(employees)
-        })
+function addEmployee() {
+    return inquirer.prompt ([
+    {
+        type: 'confirm',
+        name: 'addEmployee', 
+        message: 'Would you like to add another employee?'
+    },
+    ]).then(({ addEmployee }) => {
+        if (addEmployee) newEmployee()
+        else renderHTMLFile();
     })
 }
 
-newEmployee()
+function renderHTMLFile() {
+    //render html file
+    fs.writeFileSync('./index.html', /*html*/`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Team Profile Generator</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    </head>
+    <div class="jumbotron" style="text-align: center; border-bottom: 5px solid black; opacity : .9; color:black; text-align:center; background:red" id="jumbotron">
+    <h1 class="display-4" style="font-weight:bolder;">This is our team!</h1>
+    </div>
+    <div class="container">
+    <div class="row">
+    ${employees.map(employees => `
+        <div class = "col-md-3 text-dark" style = "margin : 5px; padding : 0; text-align : center; background-color: rgb(216, 214, 214); border-radius: 5px; border-color: black; border-style: solid;">
+        <header style="background : beige">
+        <h1>${employees.getName()}</h1>
+        ${uniqueIcon(employees)}
+        </header>
+        <p class = 'fa-solid fa-id-card'> ID NUMBER : ${employees.getId()}</p><br>
+        <p class= "fa-solid fa-envelope"><a href="mailto:${employees.getEmail()}"> Email</a></p><br>
+        ${uniqueInfo(employees)}
+        </div>
+        `)}
+        </div>
+        </div>
+    `)
+    }
+    
+    function uniqueInfo (employees) {
+        switch (employees.getRole())  {
+            // gets office number
+            case "Manager": 
+            return  `<p class= "fa-solid fa-door-open"> Office Number: ${employees.getOfficeNumber()}</p>` 
+            break;
+            // gets github account
+            case 'Engineer' : 
+            return ` <p class="fa-brands fa-github"><a href ="https://www.github.com/${employees.getGithub()}"> GitHub</a></p>`
+            break;
+            // get the school of intern
+            case 'Intern' : 
+            return `<p class="fa-solid fa-school-flag"> School : ${employees.getSchool()}</p>`
+            break;
+        }
+    }
+    
+    function uniqueIcon(employees){
+        switch(employees.getRole()) {
+        case "Manager" : 
+        return `<h3 class= "fa-solid fa-business-time"> ${employees.getRole()}</h3><br>`
+        break;
+        case 'Engineer' :
+            return `<h3 class= "fa-solid fa-laptop-code"> ${employees.getRole()}</h3><br>`
+        break;
+        case "Intern" :
+            return `<h3 class= "fa-solid fa-user"> ${employees.getRole()}</h3><br>`
+    }
+    }
+    
+    
+    newEmployee()
